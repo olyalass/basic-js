35ca72ec8f1c600833bd9e13ffdf7ef004f85f81
olyalass <olyalass@icloud.com>
2023-04-17 17:12:32 +0200

    feat: solve vigenere-cipher and what-season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,86 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet;
+  isDirect;
+
+  constructor(isDirect = true) {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+      let encripted = "";
+      let notLetters = 0;
+      for (let i = 0; i < message.length; i++) {
+        let j = i - notLetters;
+        while (j >= key.length) j -= key.length;
+        if (this.alphabet.indexOf(message[i]) !== -1) {
+          let mesCharIndex = this.alphabet.indexOf(message[i]);
+          let keyCharIndex = this.alphabet.indexOf(key[j]);
+          let encCharIndex = mesCharIndex + keyCharIndex;
+          if (encCharIndex >= this.alphabet.length)
+            encCharIndex -= this.alphabet.length;
+          encripted += this.alphabet[encCharIndex];
+        } else {
+          encripted += message[i];
+          notLetters++;
+        }
+      }
+      return this.isDirect ? encripted : encripted.split("").reverse().join("");
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.toUpperCase();
+      let decripted = "";
+      let notLetters = 0;
+      for (let i = 0; i < encryptedMessage.length; i++) {
+        let j = i - notLetters;
+        while (j >= key.length) j -= key.length;
+        if (this.alphabet.indexOf(encryptedMessage[i]) !== -1) {
+          let mesCharIndex = this.alphabet.indexOf(encryptedMessage[i]);
+          let keyCharIndex = this.alphabet.indexOf(key[j]);
+          let encCharIndex = mesCharIndex - keyCharIndex;
+          if (encCharIndex < 0) encCharIndex += this.alphabet.length;
+          decripted += this.alphabet[encCharIndex];
+        } else {
+          decripted += encryptedMessage[i];
+          notLetters++;
+        }
+      }
+      return this.isDirect ? decripted : decripted.split("").reverse().join("");
+    }
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  } else {
+    try {
+      date.getUTCSeconds();
+      const month = date.getMonth();
+      if (month < 2) {
+        return "winter";
+      } else if (month < 5) {
+        return "spring";
+      } else if (month < 8) {
+        return "summer";
+      } else if (month < 11) {
+        return "fall";
+      } else return "winter";
+    } catch (error) {
+      throw new Error("Invalid date!");
+    }
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
